28c6a173ca72a9ec62b09f5e080e3adfcf333372
unknown <exxxar@gmail.com>
2021-03-31 10:27:11 +0300

    current version@

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 
     <link href='https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
 
-    <link rel="stylesheet" href="{{asset("/css/custom.css")}}">
+ {{--   <link rel="stylesheet" href="{{asset("/css/custom.css")}}">--}}
     <!-- Styles -->
     <link
         href="{{ env("APP_DEBUG")?asset('css/desktop/app.css'):asset("css/desktop/app.min.css") }}?version={{env("APP_VERSION")}}"
