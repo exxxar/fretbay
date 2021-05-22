a57fbe3958d6d11f188c245bd1ee840eefd23a06
Michael Melikov <coutaq@gmail.com>
2021-05-22 20:52:49 +0300

    заполнил первые 2 категории

--- database/seeds/TypeOfObjectTableSeeder.php
+++ database/seeds/TypeOfObjectTableSeeder.php
@@ -20,10 +20,10 @@ class TypeOfObjectTableSeeder extends Seeder
      */
     public function run()
     {
-        //
-
         TypeOfObject::truncate();
 
+
+        //Entrance
         TypeOfObject::create([
             "title" => [
                 "en"=>"TV stand",
@@ -67,7 +67,132 @@ class TypeOfObjectTableSeeder extends Seeder
             "img" => "/images/common/icons/volume/3h.svg",
             "object_category_id" =>$this->getCategoryIdByTitle("Entrance")
         ]);
+        TypeOfObject::create([
+            "title" =>
+            [
+                "en"=>"Pedestal table",
+                "ru"=>"Pedestal table",
+                'fr'=>"Pedestal table",
+            ],
+            "img" => "/images/common/icons/volume/5h.svg",
+            "object_category_id" =>$this->getCategoryIdByTitle("Entrance")
+        ]);
+        TypeOfObject::create([
+            "title" =>
+            [
+                "en"=>"Console",
+                "ru"=>"Console",
+                'fr'=>"Console",
+            ],
+            "img" => "/images/common/icons/volume/6h.svg",
+            "object_category_id" =>$this->getCategoryIdByTitle("Entrance")
+        ]);
+        TypeOfObject::create([
+            "title" =>
+            [
+                "en"=>"Chair",
+                "ru"=>"Chair",
+                'fr'=>"Chair",
+            ],
+            "img" => "/images/common/icons/volume/30h.svg",
+            "object_category_id" =>$this->getCategoryIdByTitle("Entrance")
+        ]);
+        TypeOfObject::create([
+            "title" =>
+            [
+                "en"=>"Moving box",
+                "ru"=>"Moving box",
+                'fr'=>"Moving box",
+            ],
+            "img" => "/images/common/icons/volume/8h.svg",
+            "object_category_id" =>$this->getCategoryIdByTitle("Entrance")
+        ]);
 
 
+
+            //Bathroom
+        TypeOfObject::create([
+            "title" => [
+                "en"=>"Column cabinet",
+                "ru"=>"Column cabinet",
+                'fr'=>"Column cabinet",
+            ],
+            "img" => "/images/common/icons/volume/9h.svg",
+            "object_category_id" =>$this->getCategoryIdByTitle("Bathroom")
+        ]);
+
+
+        TypeOfObject::create([
+            "title" => [
+                "en"=>"Low cabinet",
+                "ru"=>"Low cabinet",
+                'fr'=>"Low cabinet",
+            ],
+            "img" => "/images/common/icons/volume/10h.svg",
+            "object_category_id" =>$this->getCategoryIdByTitle("Bathroom")
+        ]);
+
+
+        TypeOfObject::create([
+            "title" =>
+            [
+                "en"=>"Laundry basket",
+                "ru"=>"Laundry basket",
+                'fr'=>"Laundry basket",
+            ],
+            "img" => "/images/common/icons/volume/11h.svg",
+            "object_category_id" =>$this->getCategoryIdByTitle("Bathroom")
+        ]);
+
+        TypeOfObject::create([
+            "title" =>
+            [
+                "en"=>"Moving box",
+                "ru"=>"Moving box",
+                'fr'=>"Moving box",
+            ],
+            "img" => "/images/common/icons/volume/12h.svg",
+            "object_category_id" =>$this->getCategoryIdByTitle("Bathroom")
+        ]);
+        TypeOfObject::create([
+            "title" =>
+            [
+                "en"=>"Washing machine",
+                "ru"=>"Washing machine",
+                'fr'=>"Washing machine",
+            ],
+            "img" => "/images/common/icons/volume/13h.svg",
+            "object_category_id" =>$this->getCategoryIdByTitle("Bathroom")
+        ]);
+        TypeOfObject::create([
+            "title" =>
+            [
+                "en"=>"Dryer",
+                "ru"=>"Dryer",
+                'fr'=>"Dryer",
+            ],
+            "img" => "/images/common/icons/volume/14h.svg",
+            "object_category_id" =>$this->getCategoryIdByTitle("Bathroom")
+        ]);
+        TypeOfObject::create([
+            "title" =>
+            [
+                "en"=>"Medicine closet",
+                "ru"=>"Medicine closet",
+                'fr'=>"Medicine closet",
+            ],
+            "img" => "/images/common/icons/volume/15h.svg",
+            "object_category_id" =>$this->getCategoryIdByTitle("Bathroom")
+        ]);
+        TypeOfObject::create([
+            "title" =>
+            [
+                "en"=>"Wall shelf",
+                "ru"=>"Wall shelf",
+                'fr'=>"Wall shelf",
+            ],
+            "img" => "/images/common/icons/volume/16h.svg",
+            "object_category_id" =>$this->getCategoryIdByTitle("Bathroom")
+        ]);
     }
 }
