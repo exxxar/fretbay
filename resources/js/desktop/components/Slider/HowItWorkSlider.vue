8d03c7faa3bdad1dc84c519af0d0f4f3e6fbc003
blackice18 <sdsdfhshs.dima@yandex.ru>
2021-05-22 13:25:28 +0300

    добавлен файл scss для цветов,шрифтов +вынесен слайдер в vue компонент

---
+++ resources/js/desktop/components/Slider/HowItWorkSlider.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <section id="commentcamarche-section-tabs">
+            <div class="container">
+                <div class="tabs">
+                    <div class="tab-pane tabs__content active">
+                        <div class="slide-thumb">
+                            <img src="/images/commentcamarche-img1.png" alt="">
+                        </div>
+                        <div class="slide-content">
+                            <h2>Publish a listing</h2>
+                            <p>Post your listing for free. It is only a few clicks away from being published and visible
+                                by all our professional carriers.</p>
+                            <a href="#" class="btn-theme">REQUEST FOR QUOTATION</a>
+                        </div>
+                    </div>
+                    <div class="tab-pane tabs__content">
+                        <div class="slide-thumb">
+                            <img src="/images/commentcamarche-img2.png" alt="">
+                        </div>
+                        <div class="slide-content">
+                            <h2>Receive quotes</h2>
+                            <p>Once your listing is posted, our professional carriers will have access to your request
+                                and will offer you their quote. You will have the opportunity to choose the quote that
+                                suits you best and cheapest thanks to our reverse auction system.</p>
+                            <a href="#" class="btn-theme">REQUEST FOR QUOTATION</a>
+                        </div>
+                    </div>
+                    <div class="tab-pane tabs__content">
+                        <div class="slide-thumb">
+                            <img src="/images/commentcamarche-img3.png" alt="">
+                        </div>
+                        <div class="slide-content">
+                            <h2>Get delivered</h2>
+                            <p>Once your quotation has been selected, the carrier will take care of your furniture and
+                                belongings at the address and on the date you have chosen until the destination of your
+                                choice. Your belongings are handled safely by professionals equipped with equipment
+                                adapted to your needs in order to avoid any damage.</p>
+                            <a href="#" class="btn-theme">REQUEST FOR QUOTATION</a>
+                        </div>
+                    </div>
+                    <div class="tab-pane tabs__content">
+                        <div class="slide-thumb">
+                            <img src="/images/commentcamarche-img4.png" alt="">
+                        </div>
+                        <div class="slide-content">
+                            <h2>Evaluate the service</h2>
+                            <p>When your move is completed, you have the opportunity to evaluate your carrier on our
+                                site. All our carriers are evaluated in order to allow you to choose your service as
+                                well as possible. So, thanks to the contribution of our customers, you are guaranteed to
+                                find the carrier that suits you.</p>
+                            <a href="#" class="btn btn-theme">REQUEST FOR QUOTATION</a>
+                        </div>
+                    </div>
+                    <ul class="tabs__caption">
+                        <li class="active" >
+                            <strong>1</strong>Publish a listing
+                        </li>
+                        <li>
+                            <strong>2</strong>Receive quotes
+                        </li>
+                        <li>
+                            <strong>3</strong>Get delivered
+                        </li>
+                        <li>
+                            <strong>4</strong>Evaluate the service
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HowItWorkSlider"
+    }
+</script>
+
+<style scoped>
+
+</style>
