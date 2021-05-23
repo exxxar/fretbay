e654347b713eaf5f3e48e7460e9736c41034a234
blackice18 <sdsdfhshs.dima@yandex.ru>
2021-05-23 20:57:00 +0300

    вынесены в компоненты top movers, latest transactions, trustpilot

--- resources/js/desktop/components/TopMovers.vue
+++ resources/js/desktop/components/TopMovers.vue
@@ -3,36 +3,14 @@
         <h2>
             Top movers
         </h2>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="postBlock row">
-                    <div class="col-md-3 col-sm-4 no-padding">
-                        <div class="postImg">
-                            <img src="/images/avatar.png" alt="about" class="about__img img-responsive mx-auto d-block">
-                        </div>
-                    </div>
-                    <div class="col-md-9 col-sm-8">
-                        <p class="postblock__title">
-                            Wojtek92
-                        </p>
-                        <p class="postblock__prg">
-                            Excellent service. Un seul bémol : le problème de la langue. Il y a ..
-                        </p>
-                        <div class="ratingBlock">
-                            <img src="/images/star-on.png">
-                            <img src="/images/star-on.png">
-                            <img src="/images/star-on.png">
-                            <img src="/images/star-on.png">
-                            <img src="/images/star-half.png">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <top-mover-item />
     </div>
 </template>
 <script>
+    import TopMoverItem from "./TopMoverItem";
     export default {
-
+        components: {
+            TopMoverItem
+        }
     }
 </script>
