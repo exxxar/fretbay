444d2c2cb019857465b14169df19101c4dd62719
G-Mell <jlmelnichenko@gmail.com>
2021-05-31 00:12:40 +0300

    валидация миграция

--- database/migrations/2021_03_20_082921_create_profiles_table.php
+++ database/migrations/2021_03_20_082921_create_profiles_table.php
@@ -15,6 +15,19 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->string('email')->unique();
+            $table->string('company_name');
+            $table->string('first_name');
+            $table->string('name');
+            $table->string('telephone_number');
+            $table->string('mobile_number')->unique();
+            $table->string('country');
+            $table->string('city');
+            $table->string('region');
+            $table->string('areas_of_expertise');
+            $table->string("role_id");
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
