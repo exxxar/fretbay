7bf57539b0e15de14bfb738207be9e6f89a61fd0
unknown <exxxar@gmail.com>
2021-06-02 11:42:58 +0300

    add categories seeder@

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -12,175 +12,293 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-     Category::create([
-         'title'=>[
-             "en" => "Household equipment",
-             "ru" => "Household equipment",
-             'fr' => "Household equipment",
-         ],
-         'min_price'=>0,
-         'additional_menu_title'=>'choice_of_category',
-         'image'=>'/images/common/icons/category/default/1.svg',
-         'position'=>0,
-         'is_active'=>true,
-         'mode'=>''
-     ]);
+        Category::create([
+            'title' => [
+                "en" => "Household equipment",
+                "ru" => "Household equipment",
+                'fr' => "Household equipment",
+            ],
+            'min_price' => 0,
+            'additional_menu_title' => 'choice_of_category',
+            'image' => '/images/common/icons/category/default/1.svg',
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
+        ]);
 
         Category::create([
-            'title'=>[
+            'title' => [
                 "en" => "Moving",
                 "ru" => "Moving",
                 'fr' => "Moving",
             ],
-            'min_price'=>0,
-            'additional_menu_title'=>'choice_of_category',
-            'image'=>'/images/common/icons/category/default/2.svg',
-            'position'=>0,
-            'is_active'=>true,
-            'mode'=>''
+            'min_price' => 0,
+            'additional_menu_title' => 'choice_of_category',
+            'image' => '/images/common/icons/category/default/2.svg',
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
         ]);
 
         Category::create([
-            'title'=>[
+            'title' => [
                 "en" => "Vehicle",
                 "ru" => "Vehicle",
                 'fr' => "Vehicle",
             ],
-            'min_price'=>0,
-            'additional_menu_title'=>'choice_of_category',
-            'image'=>'/images/common/icons/category/default/3.svg',
-            'position'=>0,
-            'is_active'=>true,
-            'mode'=>''
+            'min_price' => 0,
+            'additional_menu_title' => 'choice_of_category',
+            'image' => '/images/common/icons/category/default/3.svg',
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
         ]);
 
 
         Category::create([
-            'title'=>[
+            'title' => [
                 "en" => "Motorcycles and sports vehicles",
                 "ru" => "Motorcycles and sports vehicles",
                 'fr' => "Motorcycles and sports vehicles",
             ],
-            'min_price'=>0,
-            'additional_menu_title'=>'choice_of_category',
-            'image'=>'/images/common/icons/category/default/17.svg',
-            'position'=>0,
-            'is_active'=>true,
-            'mode'=>''
+            'min_price' => 0,
+            'additional_menu_title' => 'choice_of_category',
+            'image' => '/images/common/icons/category/default/17.svg',
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
         ]);
 
         Category::create([
-            'title'=>[
+            'title' => [
                 "en" => "Boats & voluminous",
                 "ru" => "Boats & voluminous",
                 'fr' => "Boats & voluminous",
             ],
-            'min_price'=>0,
-            'additional_menu_title'=>'choice_of_category',
-            'image'=>'/images/common/icons/category/default/5.svg',
-            'position'=>0,
-            'is_active'=>true,
-            'mode'=>''
+            'min_price' => 0,
+            'additional_menu_title' => 'choice_of_category',
+            'image' => '/images/common/icons/category/default/5.svg',
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
         ]);
 
         Category::create([
-            'title'=>[
+            'title' => [
                 "en" => "Fragile goods",
                 "ru" => "Fragile goods",
                 'fr' => "Fragile goods",
             ],
-            'min_price'=>0,
-            'additional_menu_title'=>'choice_of_category',
-            'image'=>'/images/common/icons/category/default/13.svg',
-            'position'=>0,
-            'is_active'=>true,
-            'mode'=>''
+            'min_price' => 0,
+            'additional_menu_title' => 'choice_of_category',
+            'image' => '/images/common/icons/category/default/13.svg',
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
         ]);
 
         Category::create([
-            'title'=>[
+            'title' => [
                 "en" => "Package/Fold",
                 "ru" => "Package/Fold",
                 'fr' => "Package/Fold",
             ],
-            'min_price'=>0,
-            'additional_menu_title'=>'choice_of_category',
-            'image'=>'/images/common/icons/category/default/18.svg',
-            'position'=>0,
-            'is_active'=>true,
-            'mode'=>''
+            'min_price' => 0,
+            'additional_menu_title' => 'choice_of_category',
+            'image' => '/images/common/icons/category/default/18.svg',
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
         ]);
 
 
         Category::create([
-            'title'=>[
+            'title' => [
                 "en" => "Pallet",
                 "ru" => "Pallet",
                 'fr' => "Pallet",
             ],
-            'min_price'=>0,
-            'additional_menu_title'=>'choice_of_category',
-            'image'=>'/images/common/icons/category/default/19.svg',
-            'position'=>0,
-            'is_active'=>true,
-            'mode'=>''
+            'min_price' => 0,
+            'additional_menu_title' => 'choice_of_category',
+            'image' => '/images/common/icons/category/default/19.svg',
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
         ]);
 
         Category::create([
-            'title'=>[
+            'title' => [
                 "en" => "Animals",
                 "ru" => "Animals",
                 'fr' => "Animals",
             ],
-            'min_price'=>0,
-            'additional_menu_title'=>'choice_of_category',
-            'image'=>'/images/common/icons/category/default/11.svg',
-            'position'=>0,
-            'is_active'=>true,
-            'mode'=>''
+            'min_price' => 0,
+            'additional_menu_title' => 'choice_of_category',
+            'image' => '/images/common/icons/category/default/11.svg',
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
         ]);
 
 
         Category::create([
-            'title'=>[
+            'title' => [
                 "en" => "Agri-food",
                 "ru" => "Agri-food",
                 'fr' => "Agri-food",
             ],
-            'min_price'=>0,
-            'additional_menu_title'=>'choice_of_category',
-            'image'=>'/images/common/icons/category/default/14.svg',
-            'position'=>0,
-            'is_active'=>true,
-            'mode'=>''
+            'min_price' => 0,
+            'additional_menu_title' => 'choice_of_category',
+            'image' => '/images/common/icons/category/default/14.svg',
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
         ]);
 
         Category::create([
-            'title'=>[
+            'title' => [
                 "en" => "Commercial goods",
                 "ru" => "Commercial goods",
                 'fr' => "Commercial goods",
             ],
-            'min_price'=>0,
-            'additional_menu_title'=>'choice_of_category',
-            'image'=>'/images/common/icons/category/default/9.svg',
-            'position'=>0,
-            'is_active'=>true,
-            'mode'=>''
+            'min_price' => 0,
+            'additional_menu_title' => 'choice_of_category',
+            'image' => '/images/common/icons/category/default/9.svg',
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
         ]);
 
         Category::create([
-            'title'=>[
+            'title' => [
                 "en" => "Miscellaneous",
                 "ru" => "Miscellaneous",
                 'fr' => "Miscellaneous",
             ],
-            'min_price'=>0,
-            'additional_menu_title'=>'choice_of_category',
-            'image'=>'/images/common/icons/category/default/6.svg',
-            'position'=>0,
-            'is_active'=>true,
-            'mode'=>''
+            'min_price' => 0,
+            'additional_menu_title' => 'choice_of_category',
+            'image' => '/images/common/icons/category/default/6.svg',
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
         ]);
+
+        ////////////////////////////////////////////
+        ///
+
+
+        Category::create([
+            'title' => [
+                "en" => "Entrance",
+                "ru" => "Entrance",
+                'fr' => "Entrance",
+            ],
+            'min_price' => 0,
+            'additional_menu_title' => 'volume',
+            'image' => null,
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
+        ]);
+
+
+        Category::create([
+            'title' => [
+                "en" => "Bathroom",
+                "ru" => "Bathroom",
+                'fr' => "Bathroom",
+            ],
+            'min_price' => 0,
+            'additional_menu_title' => 'volume',
+            'image' => null,
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
+        ]);
+
+        Category::create([
+            'title' => [
+                "en" => "Living room",
+                "ru" => "Living room",
+                'fr' => "Living room",
+            ],
+            'min_price' => 0,
+            'additional_menu_title' => 'volume',
+            'image' => null,
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
+        ]);
+
+        Category::create([
+            'title' => [
+                "en" => "Kitchen",
+                "ru" => "Kitchen",
+                'fr' => "Kitchen",
+            ],
+            'min_price' => 0,
+            'additional_menu_title' => 'volume',
+            'image' => null,
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
+        ]);
+
+        Category::create([
+            'title' => [
+                "en" => "Dining room",
+                "ru" => "Dining room",
+                'fr' => "Dining room",
+            ],
+            'min_price' => 0,
+            'additional_menu_title' => 'volume',
+            'image' => null,
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
+        ]);
+
+        Category::create([
+            'title' => [
+                "en" => "Chamber",
+                "ru" => "Chamber",
+                'fr' => "Chamber",
+            ],
+            'min_price' => 0,
+            'additional_menu_title' => 'volume',
+            'image' => null,
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
+        ]);
+
+        Category::create([
+            'title' => [
+                "en" => "Garden",
+                "ru" => "Garden",
+                'fr' => "Garden",
+            ],
+            'min_price' => 0,
+            'additional_menu_title' => 'volume',
+            'image' => null,
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
+        ]);
+
+        Category::create([
+            'title' => [
+                "en" => "Miscellaneous",
+                "ru" => "Miscellaneous",
+                'fr' => "Miscellaneous",
+            ],
+            'min_price' => 0,
+            'additional_menu_title' => 'volume',
+            'image' => null,
+            'position' => 0,
+            'is_active' => true,
+            'mode' => ''
+        ]);
+
     }
 }
