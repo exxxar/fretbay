5959a11ba20abbc397c5818f4651ac4c8307a2fc
unknown <exxxar@gmail.com>
2021-06-13 13:24:30 +0300

    merged

--- resources/js/desktop/store/index.js
+++ resources/js/desktop/store/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import TypeOfTransportModule from './modules/TypeOfTransportModule'
-import TypeOfObjectModule from './modules/TypeOfObjectModule'
+
 import ArticleCart from './modules/ArticleCart'
 import VolumeCart from './modules/VolumeCart'
-import VehicleModule from './modules/VehicleModule'
 import Category from "./modules/Category";
 import Subcategory from "./modules/Subcategory";
 import Thing from "./modules/Thing";
@@ -17,6 +15,6 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     namespaced: true,
     modules: {
-        TypeOfTransportModule, TypeOfObjectModule, ArticleCart, VolumeCart, VehicleModule, Category, Subcategory, Thing, ListingModule, Listing
+        ArticleCart, VolumeCart, Category, Subcategory, Thing, ListingModule, Listing
     }
 })
