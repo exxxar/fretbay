b012861b2196a1de80ad6c686406c2f4ee6d103f
Mona Lisa <exxxar@gmail.com>
2021-12-09 18:57:38 +0300

    modified admin panel

--- resources/lang/en/menu.php
+++ resources/lang/en/menu.php
@@ -1,32 +1,33 @@
 <?php
 
-return array (
-  'item' => 
-  array (
-    'faq' => 'Frequently Ask Questions',
-    'find_loads' => 'Find loads',
-    'how_does_it_work' => 'How does it work',
-    'i_am_a_transporter' => 'I am a transporter',
-    'privacy' => 'Privacy',
-    'request_a_quote' => 'Request a quote',
-    'search_for_a_transporter' => 'Search for a transporter',
-    'sign_in' => 'SignIn',
-    'sign_up' => 'SignUp',
-    'sign_up_customer' => 'SignUp as customer',
-    'sign_up_transporter' => 'SignUp as transporter',
-    'who_we_are' => 'Who we are',
-  ),
-  'section' => 
-  array (
-    'about' => 'About',
-    'custom_service' => 'Custom Service',
-    'customer_service' => 'Service client',
-    'help' => 'Help',
-    'social' => 'Social',
-  ),
-  'text' => 
-  array (
-    'all_rights_reserved' => 'All right reserved(c)ALLOTRANS',
-    'search' => 'Search in Inventory',
-  ),
+return array(
+    'item' =>
+        array(
+            'faq' => 'Frequently Ask Questions',
+            'find_loads' => 'Find loads',
+            'how_does_it_work' => 'How does it work',
+            'i_am_a_transporter' => 'I am a transporter',
+            'privacy' => 'Privacy',
+            'request_a_quote' => 'Request a quote',
+            'search_for_a_transporter' => 'Search for a transporter',
+            'sign_in' => 'SignIn',
+            'sign_up' => 'SignUp',
+            'sign_up_customer' => 'SignUp as customer',
+            'sign_up_transporter' => 'SignUp as transporter',
+            'who_we_are' => 'Who we are',
+            'posts' => "Blogs"
+        ),
+    'section' =>
+        array(
+            'about' => 'About',
+            'custom_service' => 'Custom Service',
+            'customer_service' => 'Service client',
+            'help' => 'Help',
+            'social' => 'Social',
+        ),
+    'text' =>
+        array(
+            'all_rights_reserved' => 'All right reserved(c)ALLOTRANS',
+            'search' => 'Search in Inventory',
+        ),
 );
--- resources/lang/fr/menu.php
+++ resources/lang/fr/menu.php
@@ -1,32 +1,33 @@
 <?php
 
-return array (
-  'item' => 
-  array (
-    'faq' => 'Foire aux questions',
-    'find_loads' => 'Rechercher des charges',
-    'how_does_it_work' => 'Comment ça marche',
-    'i_am_a_transporter' => 'Je suis un transporteur',
-    'privacy' => 'Confidentialité',
-    'request_a_quote' => 'Demander un devis',
-    'search_for_a_transporter' => 'Rechercher un transporteur',
-    'sign_in' => 'Se connecter',
-    'sign_up' => 'S\'inscrire',
-    'sign_up_customer' => 'S\'inscrire en tant que client',
-    'sign_up_transporter' => 'S\'inscrire en tant que transporteur',
-    'who_we_are' => 'Qui nous sommes',
-  ),
-  'section' => 
-  array (
-    'about' => 'À propos de',
-    'custom_service' => 'Custom Service',
-    'customer_service' => 'Service client',
-    'help' => 'Aide',
-    'social' => 'Social',
-  ),
-  'text' => 
-  array (
-    'all_rights_reserved' => 'Tous droits réservés(c) AlloTrans',
-    'search' => 'Rechercher dans l\'inventaire',
-  ),
+return array(
+    'item' =>
+        array(
+            'faq' => 'Foire aux questions',
+            'find_loads' => 'Rechercher des charges',
+            'how_does_it_work' => 'Comment ça marche',
+            'i_am_a_transporter' => 'Je suis un transporteur',
+            'privacy' => 'Confidentialité',
+            'request_a_quote' => 'Demander un devis',
+            'search_for_a_transporter' => 'Rechercher un transporteur',
+            'sign_in' => 'Se connecter',
+            'sign_up' => 'S\'inscrire',
+            'sign_up_customer' => 'S\'inscrire en tant que client',
+            'sign_up_transporter' => 'S\'inscrire en tant que transporteur',
+            'who_we_are' => 'Qui nous sommes',
+            'posts' => "Blogs"
+        ),
+    'section' =>
+        array(
+            'about' => 'À propos de',
+            'custom_service' => 'Custom Service',
+            'customer_service' => 'Service client',
+            'help' => 'Aide',
+            'social' => 'Social',
+        ),
+    'text' =>
+        array(
+            'all_rights_reserved' => 'Tous droits réservés(c) AlloTrans',
+            'search' => 'Rechercher dans l\'inventaire',
+        ),
 );
